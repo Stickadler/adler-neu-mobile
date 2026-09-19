c8872c1e9bc4b6dd564f8dba67f337ae80ee6472
Stickadler <daniel@stickadler.de>
2026-09-19 19:41:58 +0200

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE='adler-mobile-v1';const CORE=['/','/index.html','/manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
